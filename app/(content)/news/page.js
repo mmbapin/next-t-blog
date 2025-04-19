f67907fc6d1d99a2e8dfba638a16dbd52095b3ac
mmbapin <bapinmm@gmail.com>
2025-04-20 00:53:55 +0600

    server side data fetching done

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news/news-list';
-const NewsPage = () => {
+const NewsPage = async () => {
+	const response = await fetch('http://localhost:8080/news');
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch news data!');
+	}
+
+	const news = await response.json();
+
 	return (
 		<>
 			<h1>News Page</h1>
-			<NewsList news={DUMMY_NEWS} />
+			<NewsList news={news} />
 		</>
 	);
 };
